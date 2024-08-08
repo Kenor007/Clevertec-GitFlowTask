98ecae414dcf997acd5396656174675e26846939
Evgeniy Kanareikin <kenor@tut.by>
2024-08-08 23:53:05 +0300

    Small fixes

--- src/main/java/ru/clevertec/Main.java
+++ src/main/java/ru/clevertec/Main.java
@@ -2,7 +2,5 @@ package main.java.ru.clevertec;
 
 public class Main {
     public static void main(String[] args) {
-        Engine engine = new Engine("SEDAN", 240, 5);
-        Car car = new Car();
     }
 }
